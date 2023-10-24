b530fac804de745b6b636f87aeb3a25272c100d1
camargo <gui.ferreira.camargo@gmail.com>
2023-10-24 18:27:31 -0300

    Model Page

--- djangoapp/blog/models.py
+++ djangoapp/blog/models.py
@@ -21,6 +21,9 @@ class Tag(models.Model):
             self.slug = new_slugify(self.name, 4)
         return super().save(*args, **kwargs)
 
+    def __str__(self) -> str:
+        return self.name
+
 
 class Category(models.Model):
     class Meta:
@@ -40,3 +43,33 @@ class Category(models.Model):
         if not self.slug:
             self.slug = new_slugify(self.name, 4)
         return super().save(*args, **kwargs)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Page(models.Model):
+    title = models.CharField(max_length=65,)
+    slug = models.SlugField(
+        unique=True,
+        default="",
+        null=True,
+        blank=True,
+        max_length=255,
+    )
+    is_published = models.BooleanField(
+        default=False,
+        help_text=(
+            'Este campo precisa estar marcado para '
+            'que a página seja exibida publicamente.'
+        )
+    )
+    content = models.TextField()
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = new_slugify(self.title, 4)
+        return super().save(*args, **kwargs)
+
+    def __str__(self) -> str:
+        return self.title
