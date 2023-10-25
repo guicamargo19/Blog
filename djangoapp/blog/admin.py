972d9cc4ae199cbd56f164837b7f4a8946c09cdc
camargo <gui.ferreira.camargo@gmail.com>
2023-10-25 14:47:58 -0300

    Model Post

--- djangoapp/blog/admin.py
+++ djangoapp/blog/admin.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
-from blog.models import Tag, Category, Page
+from blog.models import Tag, Category, Page, Post
 
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
     list_display = 'id', 'name', 'slug',
     list_display_links = 'name',
-    searh_fields = 'id', 'name', 'slug',
+    search_fields = 'id', 'name', 'slug',
     list_per_page = 10
     ordering = '-id',
     prepopulated_fields = {
@@ -18,7 +18,7 @@ class TagAdmin(admin.ModelAdmin):
 class CategoryAdmin(admin.ModelAdmin):
     list_display = 'id', 'name', 'slug',
     list_display_links = 'name',
-    searh_fields = 'id', 'name', 'slug',
+    search_fields = 'id', 'name', 'slug',
     list_per_page = 10
     ordering = '-id',
     prepopulated_fields = {
@@ -30,7 +30,7 @@ class CategoryAdmin(admin.ModelAdmin):
 class PageAdmin(admin.ModelAdmin):
     list_display = 'id', 'title', 'is_published',
     list_display_links = 'title',
-    searh_fields = 'id', 'slug', 'title', 'content',
+    search_fields = 'id', 'slug', 'title', 'content',
     list_per_page = 10
     list_filter = 'is_published',
     list_editable = 'is_published',
@@ -38,3 +38,19 @@ class PageAdmin(admin.ModelAdmin):
     prepopulated_fields = {
         "slug": ('title',),
     }
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = 'id', 'title', 'is_published', 'created_by',
+    list_display_links = 'title',
+    search_fields = 'id', 'slug', 'title', 'excerpt', 'content',
+    list_per_page = 50
+    list_filter = 'category', 'is_published',
+    list_editable = 'is_published',
+    ordering = '-id',
+    readonly_fields = 'created_at', 'updated_at', 'updated_by', 'created_by',
+    prepopulated_fields = {
+        "slug": ('title',),
+    }
+    autocomplete_fields = 'tags', 'category',
