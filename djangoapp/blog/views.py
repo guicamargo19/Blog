8c62ed28ab64a2805fb2d2990316c63d9d03f4a8
camargo <gui.ferreira.camargo@gmail.com>
2023-10-28 14:19:24 -0300

    Created By Class View

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -10,10 +10,8 @@ PER_PAGE = 9
 
 
 class PostListView(ListView):
-    model = Post
     template_name = 'blog/pages/index.html'
     context_object_name = 'posts'
-    ordering = '-pk',
     paginate_by = PER_PAGE
     queryset = Post.objects.get_published()  # type: ignore
 
@@ -73,7 +71,7 @@ def page(request, slug):
     )
 
 
-def created_by(request, author_pk):
+""" def created_by(request, author_pk):
     user = User.objects.filter(pk=author_pk).first()
 
     if user is None:
@@ -100,7 +98,49 @@ def created_by(request, author_pk):
             'page_obj': page_obj,
             'page_title': page_title,
         }
-    )
+    ) """
+
+
+class CreatedByListView(PostListView):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self._temp_context = {}
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        user = self._temp_context['user']
+        user_full_name = user.username
+
+        if user.first_name:
+            user_full_name = f'{user.first_name} {user.last_name}'
+        page_title = 'Posts de ' + user_full_name + ' - '
+
+        ctx.update(
+            {
+                'page_title': page_title,
+            }
+        )
+
+        return ctx
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        qs = qs.filter(created_by__pk=self._temp_context['user'].pk)
+        return qs
+
+    def get(self, request, *args, **kwargs):
+        author_pk = self.kwargs.get('author_pk')
+        user = User.objects.filter(pk=author_pk).first()
+
+        if user is None:
+            raise Http404()
+
+        self._temp_context.update({
+            'author_pk': author_pk,
+            'user': user,
+        })
+
+        return super().get(request, *args, **kwargs)
 
 
 def category(request, slug):
